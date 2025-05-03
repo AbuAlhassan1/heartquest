9fe2083a9b8e88c9cf3599a1cc827eb3aa12fc26
AbuAlhassan1 <abualhassanbasim@gmail.com>
2025-05-04 00:05:08 +0300

    Add GitHub Actions workflow for deployment and update configuration files

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export',
+  // Indicate that the app should be deployed at /heartquest
+  basePath: process.env.NODE_ENV === 'production' ? '/heartquest' : '',
 }
 
 export default nextConfig
